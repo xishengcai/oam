649bc16d58ef7bb6ad611af18feb4083c833dc65
caixisheng <cc710917049@163.c0m>
2021-12-07 18:15:59 +0800

    refactor(root): 修改 size if null； 1Gi

--- pkg/controller/v1alpha2/core/traits/volumeclaimtrait/handler.go
+++ pkg/controller/v1alpha2/core/traits/volumeclaimtrait/handler.go
@@ -18,13 +18,19 @@ type VolumeClaimHandler struct {
 }
 
 func (v VolumeClaimHandler) Create(event event.CreateEvent, limitingInterface workqueue.RateLimitingInterface) {
+
 }
 
 func (v VolumeClaimHandler) Update(event event.UpdateEvent, limitingInterface workqueue.RateLimitingInterface) {
+
 }
 
 func (v VolumeClaimHandler) Delete(event event.DeleteEvent, limitingInterface workqueue.RateLimitingInterface) {
+
 }
 
 func (v VolumeClaimHandler) Generic(event event.GenericEvent, limitingInterface workqueue.RateLimitingInterface) {
+	// Generic is called in response to an event of an unknown type or a synthetic event triggered as a cron or
+	// external trigger request - e.g. reconcile Autoscaling, or a Webhook.
+	// so we need to do nothing here.
 }
